c9694779be18dcd313daf093774c4cf4698a71f1
kilala-mega <80940088+kilala-mega@users.noreply.github.com>
2021-06-18 00:44:28 -0700

    Update cycle-sort.py

--- cycle-sort.py
+++ cycle-sort.py
@@ -2,6 +2,9 @@ def cycleSort(nums: List[int]) -> List[int]:
     index = 0
     while index < len(nums):
         j = nums[index]-1
+        # it's better to use the condition nums[index] != nums[j]
+        # rather than comparing j and i directly
+        # o.w. the algo might infinite loop to detect duplicates
         if nums[index] != nums[j]:
             nums[index], nums[j] = nums[j], nums[index]
         else:
